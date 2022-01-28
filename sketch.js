62145a900666ef773a13217d70a24661930dac95
jholorobelo <jholorobeloboy@gmail.com>
2022-01-28 15:33:07 +0800

    a

---
+++ sketch.js
@@ -0,0 +1,108 @@
+let video;
+let label = "checking";
+let classifier;
+let modelURL = 'https://teachablemachine.withgoogle.com/models/wYPw7LJI_/';
+
+var capture;
+let switchFlag = false;
+let switchBtn;
+  let flippedVideo;
+ var constraints = {
+  video: {
+    
+    facingMode: 'environment'  
+  }};
+// STEP 1: Load the model!
+function preload() {
+  classifier = ml5.imageClassifier(modelURL+ 'model.json');
+}
+
+function setup() {
+  // size of vid
+  var canvas = createCanvas(500, 400);
+  canvas.parent('canvasDiv');
+  
+
+ /*var switchBtn = createButton('Switch Camera');
+  switchBtn.parent('SwitchButton');
+ //switchBtn.position(905, 800);
+ switchBtn.mousePressed(switchCamera);*/
+  
+  // Create the video
+  video = createCapture(constraints);
+  video.hide();
+
+  // STEP 2: Start classifying
+  classifyVideo();
+}
+// STEP 2 classify the videeo!
+function classifyVideo() {
+  classifier.classify(video, gotResults);
+}
+
+function draw() {
+  background(0);
+
+  
+  image(video, 0, 0);
+
+  
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(label, width / 2, height - 16);
+
+ 
+}
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+  
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // Storing the label and classifying again!
+  label = results[0].label;
+  classifyVideo();
+}
+/*function switchCamera()
+{
+  switchFlag = !switchFlag;
+  stopCapture();
+  if(switchFlag==true)
+  {
+   video.remove();
+   hello = {
+     video: {
+         facingMode: {
+          exact: "environment"
+        }
+     }
+   };
+
+  }
+  else
+  {
+   video.remove();
+   hello = {
+     video: {
+         facingMode: {
+          exact: "user"
+        }
+     }
+   };
+  }
+  video = createCapture(hello);
+  video.hide();
+}
+
+function stopCapture() {
+  let stream = video.elt.srcObject;
+  let tracks = stream.getTracks();
+
+  tracks.forEach(function(track) {
+    track.stop();
+  });
+
+ video.elt.srcObject = null;
+}*/
